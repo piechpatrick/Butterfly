fc33b52d15b904485ecc84fd23a28749aa37250d
Patrick Piech <piechpatrick@gmail.com>
2019-01-12 22:05:59 +0100

    Added Xamarin Forms projects

---
+++ Butterfly/src/Butterfly.Xamarin/Butterfly.Xamarin/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using Butterfly.Xamarin.Models;
+
+namespace Butterfly.Xamarin.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
