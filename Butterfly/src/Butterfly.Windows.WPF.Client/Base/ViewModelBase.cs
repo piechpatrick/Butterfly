f5d381a42031b5c5e0d9212adedb87bb92d29be2
Patrick Piech <piechpatrick@gmail.com>
2019-01-17 14:30:50 +0100

    +wpf client +unity container mixed with ms dependency injection principals

---
+++ Butterfly/src/Butterfly.Windows.WPF.Client/Base/ViewModelBase.cs
@@ -0,0 +1,37 @@
+﻿using Butterfly.MultiPlatform.Packets.Pings;
+using Butterfly.Windows.Handlers.WPFClient;
+using Butterfly.Windows.WPF.Client.Core.Client;
+using Networker.Client.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Butterfly.Windows.WPF.Client.ViewModels
+{
+    public class ViewModelBase
+    {
+        protected readonly IButterflyWPFClient wpfClient;
+        protected readonly INetworkClient networkClient;
+        protected readonly WPFPingPacketHandler pingPacket;
+
+        public ViewModelBase(IButterflyWPFClient wpfClient, INetworkClient netClient, WPFPingPacketHandler pingPacket)
+        {
+            this.wpfClient = wpfClient;
+            this.pingPacket = pingPacket;
+            this.networkClient = netClient;
+            this.pingPacket.OnPacketRecived += PingPacket_OnPacketRecived;
+        }
+
+        private void PingPacket_OnPacketRecived(object sender, InjectablePacketEventArgs<PingPacket, Networker.Common.Abstractions.ISender> e)
+        {
+            this.OnPingPacketRecive(e);
+        }
+
+        protected virtual void OnPingPacketRecive(InjectablePacketEventArgs<PingPacket, Networker.Common.Abstractions.ISender> ping)
+        {
+
+        }
+    }
+}
---
+++ Butterfly/src/Butterfly.Windows.WPF.Client/MainWindow/MainWindowViewModel.cs
@@ -0,0 +1,36 @@
+﻿using Butterfly.MultiPlatform.Packets.Pings;
+using Butterfly.Windows.Handlers.WPFClient;
+using Butterfly.Windows.WPF.Client.Core.Client;
+using Butterfly.Windows.WPF.Client.ViewModels;
+using Networker.Client.Abstractions;
+using Prism.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Butterfly.Windows.WPF.Client
+{
+    public class MainWindowViewModel : ViewModelBase
+    {       
+        public MainWindowViewModel(IButterflyWPFClient wpfClient, INetworkClient netClient, WPFPingPacketHandler pingPacket)
+            :base(wpfClient,netClient,pingPacket)
+        {
+            
+            this.wpfClient.Start();
+
+            
+            this.ClickCommand = new DelegateCommand(this.Send);
+            
+        }
+
+        public ICommand ClickCommand { get; private set; }
+
+        private void Send()
+        {
+            this.networkClient.SendUdp<PingPacket>(new PingPacket());
+        }
+    }
+}
